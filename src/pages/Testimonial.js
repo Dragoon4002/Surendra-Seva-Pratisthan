1577cc1888fc4a1cc813e5332a1bab14376bca5c
Dragoon4002 <sounak.dey2468@gmail.com>
2024-07-28 22:07:36 +0530

    Testimonial Page Base Creation

--- src/pages/Testimonial.js
+++ src/pages/Testimonial.js
@@ -4,9 +4,40 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const Testimonial = () => {
+  const writing_img_src = [];
+  const news_img_src = [];
   return (
-    <main>
+    <main className='testimonial'>
         <Header/>
+        <h1>Here is a list of some of the reviews and Testimonials by others</h1>
+        <div className='video'>
+          <h2>A video documentation</h2>
+        </div>
+        <div className='writing'>
+          <h2>Written Testimonial</h2>
+          <ul>
+            {writing_img_src.map(image=>{
+              return(
+                <li>
+                  <img src={image} alt='Writen Testimonials in govt documents'/>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className='article'>
+        <h2>News Articles</h2>
+          <ul>
+            {news_img_src.map(image=>{
+              return(
+                <li>
+                  <img src={image} alt='Articles on Us in Newspapers'/>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className=''></div>
         <Footer/>
     </main>
   )
